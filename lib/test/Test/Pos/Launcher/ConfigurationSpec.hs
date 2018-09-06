cdd260fee08dca7eca7dff05ef8e270d8cd15d22
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2018-09-06 23:47:36 +0300

    [CBR-275] Added logging initialization when wlogTrace is used.

--- lib/test/Test/Pos/Launcher/ConfigurationSpec.hs
+++ lib/test/Test/Pos/Launcher/ConfigurationSpec.hs
@@ -11,11 +11,14 @@ import           Pos.Core.Slotting (Timestamp (..))
 import           Pos.Launcher.Configuration (ConfigurationOptions (..),
                      defaultConfigurationOptions, withConfigurationsM)
 import           Pos.Util.Config (ConfigurationException)
+import           Pos.Util.Log.LoggerConfig (defaultTestConfiguration)
+import           Pos.Util.Wlog (Severity (Debug), setupLogging)
 
 spec :: Spec
 spec = describe "Pos.Launcher.Configuration" $ do
     describe "withConfigurationsM" $ do
         it "should parse `lib/configuration.yaml` file" $ do
+            liftIO $ setupLogging (defaultTestConfiguration Debug)
             startTime <- Timestamp . round . (* 1000000) <$> liftIO getPOSIXTime
             let cfo = defaultConfigurationOptions
                         { cfoFilePath = "./configuration.yaml"
@@ -27,4 +30,3 @@ spec = describe "Pos.Launcher.Configuration" $ do
                 (withConfigurationsM "test" Nothing cfo id (\_ _ _ -> return Nothing))
                 catchFn
             res `shouldSatisfy` isNothing
-
